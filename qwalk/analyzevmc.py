5a41ae26c0bf0ee899b3c10e8e4cf844f477025c
Shivesh Ananda Pathak <sapatha2@h2ologin3.ncsa.illinois.edu>
2018-07-30 20:59:03 -0500

    Analyzevmc cleaned

--- qwalk/analyzevmc.py
+++ qwalk/analyzevmc.py
@@ -12,22 +12,32 @@ method='B3LYP'
 #Energies
 E=[]
 err=[]
+nblock=[]
 for i in range(1,N+1):
   e=[]
   esig=[]
+  nb=0
   basename=el+basis+str(charge)+"_"+method+".vmc"+str(i)
   with open("/scratch/sciteam/sapatha2/"+basename+"/"+basename+".o","r") as f:
     for line in f:
       if("##0" in line):
 	e.append(float(line.split(" ")[10]))	
+        nb+=1
       if("&&0" in line):
 	esig.append(float(line.split(" ")[11]))	
   e=np.array(e)
   esig=np.array(esig)
-  E.append(np.sum(e)/len(e))
-  err.append(np.sqrt(np.sum(esig**2)/len(e)**2))
+   
+  if(i==5): print(esig)
+
+  e=np.sum(e)/len(e)
+  esig=np.sum(esig**2)/len(esig)**2  
+  E.append(e)
+  err.append(esig)
+  nblock.append(nb)
+  print(e,esig)
   f.close() 
 
 
-d={'E':E,'err':err,'nblock':len(e)}
+d={'E':E,'err':err,'nblock':nblock}
 json.dump(d,open("analyzevmc.json","w"))
