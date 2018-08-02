5be43045be1a06a7cbb4f9644fb44b59cf50f531
Shivesh Ananda Pathak <sapatha2@h2ologin3.ncsa.illinois.edu>
2018-08-02 10:51:18 -0500

    Added different cutoff calculations

--- qwalk/analyzedmc.py
+++ qwalk/analyzedmc.py
@@ -10,28 +10,33 @@ minao={}
 basis='vtz'
 method='B3LYP'
 
-'''
 #Energies
 E=[]
 err=[]
-for i in range(1,N+1):
-  e=[]
-  esig=[]
-  nb=0
-  basename=el+basis+str(charge)+"_"+method+".dmc_E"+str(i)
-  with open("/scratch/sciteam/sapatha2/"+basename+"/"+basename+".o","r") as f:
-    for line in f:
-      if "total_energy0" in line:
-	E.append(-1*float(line.split("-")[1][:-3]))
-	err.append(float(line.split("-")[2].split("(")[0]))
-  f.close() 
+cutoff=[]
+for cut in ["0p2","","0p4"]:
+  for i in range(1,N+1):
+    e=[]
+    esig=[]
+    nb=0
+    basename=el+basis+str(charge)+"_"+method+".dmc_E"+str(i)+cut
+    with open("/scratch/sciteam/sapatha2/"+basename+"/"+basename+".o","r") as f:
+      for line in f:
+        if "total_energy0" in line:
+	  E.append(-1*float(line.split("-")[1][:-3]))
+	  err.append(float(line.split("-")[2].split("(")[0]))
+          if(cut=="0p2"): cutoff.append(0.2)
+	  elif(cut==""): cutoff.append(0.3)
+	  else: cutoff.append(0.4)
+    f.close() 
 
 #Ground state
 E.append(-213.4851715)
 err.append(0.0005193355127)
+cutoff.append(0)
 assert(min(E)==-213.4851715)
 
-d={'E':E,'err':err}
+d={'E':E,'err':err,'cutoff':cutoff}
 json.dump(d,open("analyzedmc.json","w"))
 
 '''
@@ -47,3 +52,4 @@ d['E']*=27.2
 d['err']*=27.2
 plt.errorbar(np.arange(len(d['E'])),d['E'],yerr=d['err'],c='g',marker='o')
 plt.show()
+'''
