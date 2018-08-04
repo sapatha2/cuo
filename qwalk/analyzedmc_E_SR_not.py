756f5e8660eb654e2ed6a83087d8b852438b53ce
Shivesh Ananda Pathak <sapatha2@h2ologin2.ncsa.illinois.edu>
2018-08-04 17:48:31 -0500

    Extended analyzedmc_E_SR to include wf derivatives and total energy derivatives

--- qwalk/analyzedmc_E_SR_not.py
+++ qwalk/analyzedmc_E_SR_not.py
@@ -9,7 +9,7 @@ charge=0
 minao={}
 basis='vtz'
 method='B3LYP'
-'''
+
 #Energies
 E=[]
 err=[]
@@ -36,10 +36,48 @@ err.append(0.0005193355127)
 cutoff.append(0)
 assert(min(E)==-213.4851715)
 
-d={'E':E,'err':err,'cutoff':cutoff}
-json.dump(d,open("analyzedmc_E_SR_not.json","w"))
-'''
+#Derivatives
+wfnderiv=[]
+wfnderiv_err=[]
+ederiv=[]
+ederiv_err=[]
+cutoff=[]
+for cut in ["0p2","0p3","0p4"]:
+  for i in range(1,N+1):
+    tmp_wfnderiv=[]
+    tmp_wfnderiv_err=[]     
+    tmp_ederiv=[]
+    tmp_ederiv_err=[]     
+    basename=el+basis+str(charge)+"_"+method+".dmc_E_SR_"+str(i)+cut+"_not"
+    with open("/scratch/sciteam/sapatha2/"+basename+"/"+basename+".o","r") as f:
+      for line in f:
+        if "Wave function derivatives" in line:
+          for j in range(N):
+	    s=f.next().split(" ")
+            tmp_wfnderiv.append(float(s[0]))
+	    tmp_wfnderiv_err.append(float(s[2][:-1]))
+        if "Derivative of the energy" in line:
+          for j in range(N):
+	    s=f.next().split(" ")
+            tmp_ederiv.append(float(s[0]))
+	    tmp_ederiv_err.append(float(s[2][:-1]))
+    f.close() 
 
+    wfnderiv.append(tmp_wfnderiv)
+    wfnderiv_err.append(tmp_wfnderiv_err)
+    ederiv.append(tmp_ederiv)
+    ederiv_err.append(tmp_ederiv_err)      
+
+    if(cut=="0p2"): cutoff.append(0.2)
+    elif(cut=="0p3"): cutoff.append(0.3)
+    else: cutoff.append(0.4)
+
+d={'E':E,'err':err,'wfnderiv':wfnderiv,'wfnderiv_err':wfnderiv_err,
+'ederiv':ederiv,'ederiv_err':ederiv_err,'cutoff':cutoff}
+json.dump(d,open("analyzedmc_E_SR_not.json","w"))
+
+#Plot energy
+'''
 import matplotlib.pyplot as plt
 import pandas as pd
 d=json.load(open("analyzedmc_E_SR_not.json","r"))
@@ -66,3 +104,4 @@ plt.ylabel("E-E[GS], eV")
 plt.xlabel("State")
 plt.legend(loc=2)
 plt.show()
+'''
