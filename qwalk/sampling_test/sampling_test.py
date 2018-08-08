863ab07231a69abc7883fc96fbc1e4690bb33683
Shivesh Pathak <sapatha2@illinois.edu>
2018-08-08 11:45:08 -0700

    Finished slater generation file for sampling_testing

---
+++ qwalk/sampling_test/sampling_test.py
@@ -0,0 +1,73 @@
+#Generation for sampling files
+#Loop nchoose 
+#  Loop sign
+#    Loop nsample
+#      Loop GS weights 
+#        DMC
+
+import numpy as np 
+import random 
+import os 
+
+nchooses=[10] #subset of determinants to choose 
+nsigns=1 #number of distinct sign choices
+nsamples=10 #number of samples of nchoose determinants
+
+nup=8 #number of up electrons
+ndn=7 #number of down electrons
+actmin=6 #active space minimum
+actmax=14 #active space maximum
+
+for nchoose in nchooses:
+  for i in range(nsigns):
+    sign=[1]+(2*np.random.randint(0,2,nchoose)-1).tolist() #sign choice
+    
+    #Make working directory
+    ind=i
+    directory="./nchoose"+str(nchoose)+"_sgn"+str(ind)
+    while(os.path.exists(directory)):
+      ind+=1
+      directory="./nchoose"+str(nchoose)+"_sgn"+str(ind)
+    os.makedirs(directory)
+    os.system("cp ./Cuvtz0_B3LYP.* "+directory)
+
+    for nsample in range(nsamples):
+      up=[list(range(1,actmin))+random.sample(range(actmin,actmax+1), nup) for j in range(nchoose)] #determinant choices up
+      dn=[list(range(1,actmin))+random.sample(range(actmin,actmax+1), ndn) for j in range(nchoose)] #determinant choices down
+      assert(len(set(tuple(k) for k in up))==nchoose) #make sure you don't have repeats
+      assert(len(set(tuple(k) for k in dn))==nchoose) #make sure you don't have repeats
+      
+      for gsw in np.arange(0.1,1.0,0.1):
+        w=np.array(sign[:]).astype(float) #determinant weights
+        w[0]*=(gsw**0.5)
+        w[1:]*=((1-gsw)/nchoose)**0.5
+        assert((np.dot(w,w)-1.0)<1e-12) #make sure it's normalized
+        assert(w[0]==gsw**0.5)          #make sure you have the right first element
+
+        #Generate slater files
+        slatf="Cuvtz0_B3LYP_s"+str(nsample)+"_g"+str(gsw)+".slater"
+        f=open(directory+"/"+slatf,"w")
+        w=[str(x) for x in w]
+        up=[[str(y) for y in x] for x in up]
+        dn=[[str(y) for y in x] for x in dn]
+        
+        states=""
+        for p in range(nchoose):
+          states+=" ".join(up[p])+"\n"+" ".join(dn[p])+"\n\n"
+        print(states)
+
+        content=\
+        "SLATER\n"+\
+        "ORBITALS  {\n"+\
+        "CUTOFF_MO\n"+\
+        "MAGNIFY 1.0\n"+\
+        "NMO 14\n"+\
+        "ORBFILE Cuvtz0_B3LYP.orb\n"+\
+        "INCLUDE Cuvtz0_B3LYP.basis\n"+\
+        "CENTERS { USEGLOBAL }\n"+\
+        "}\n"+\
+        "DETWT { "+" ".join(w)+" }\n"+\
+        "STATES {\n"+states+\
+        "}\n"
+        f.write(content)
+
