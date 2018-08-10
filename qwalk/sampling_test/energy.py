bc6b2d562d8f03fb663b3bc612a29f240da31be4
Shivesh Pathak <sapatha2@illinois.edu>
2018-08-09 18:52:05 -0700

    All 1rdm + energy calculations complete, need to complete anlaysis

--- qwalk/sampling_test/energy.py
+++ qwalk/sampling_test/energy.py
@@ -27,5 +27,24 @@ d={'E':E,'Err':Err,'G':G,'S':S,'sgn':sgn,'nchoose':nchoose}
 json.dump(d,open("energy.json","w"))
 '''
 
-d=json.load(open("energy.json","w"))
+import pandas as pd
+import matplotlib.pyplot as plt
+
+d=json.load(open("energy.json","r"))
+df=pd.DataFrame(d)
+
+#Ground state
+E0=-213.361501
+err0=0.001006581014
+df['E']-=E0
+df['E']*=27.2
+df['Err']*=27.2
+
+for S in range(max(d['S'])):
+  ind=(df['S']==S)
+  plt.errorbar(df['G'][ind]*0.1,df['E'][ind],yerr=df['Err'][ind],marker='o')
+
+plt.ylabel("E(VMC) - E0(VMC), eV")
+plt.xlabel("Ground state weight")
+plt.show()
 
