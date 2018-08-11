b1175adcf1b2e57035d92e36d832493af5805fa3
Shivesh Pathak <sapatha2@illinois.edu>
2018-08-10 22:36:29 -0700

    ED analysis, didnt get the errorbars yet because of correlations

--- qwalk/sampling_test/nchoose10_sgn1/plot_data.py
+++ qwalk/sampling_test/nchoose10_sgn1/plot_data.py
@@ -9,6 +9,7 @@ sns.set_style("ticks")
 
 
 #Energy
+'''
 plt.title("VMC Energy")
 E0=-213.361501
 err0=0.001006581014
@@ -18,18 +19,19 @@ df['Err']*=27.2
 g=sns.FacetGrid(hue='S',data=df,sharey=False)
 g.map(plt.errorbar,'G','E','Err',marker='o')
 plt.show()
+'''
 
 #Parameter derivatives
+'''
 plt.title("Parameter derivatives")
 g=sns.FacetGrid(hue='S',col='Nd',data=df,sharey=False)
 g.map(plt.errorbar,'G','Pd','Pderr',marker='o')
 plt.show()
+'''
 
 #Energy derivatives
-'''
 df['Ed']=2*df['Ed']-df['E']*df['Pd']
 #df['Ederr']= #THIS IS THE HARD THING TO DO
 g=sns.FacetGrid(hue='S',col='Nd',data=df,sharey=False)
 g.map(plt.errorbar,'G','Ed','Ederr',marker='o')
 plt.show()
-'''
