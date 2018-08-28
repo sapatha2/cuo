7ff3589924874e33c8174172c9a1eeecf208c725
Shivesh Ananda Pathak <sapatha2@h2ologin2.ncsa.illinois.edu>
2018-08-28 11:36:11 -0500

    analyze complete

--- qwalk/sampling_test/nchoose10_sgn2/analyze.py
+++ qwalk/sampling_test/nchoose10_sgn2/analyze.py
@@ -1,9 +1,10 @@
 #Analyze data 
 import numpy as np 
 import sys
-#from analyze_jsonlog import compute_and_save
+#sys.path.append('/Users/shiveshpathak/Box Sync/Research/Work/si_model_fitting')
+sys.path.append('/u/sciteam/sapatha2/si_model_fitting')
+from analyze_jsonlog import compute_and_save
 
-sys.path.append('/Users/shiveshpathak/Box Sync/Research/Work/si_model_fitting')
 fnames=["Cuvtz0_B3LYP_s"+str(i)+"_g0."+str(j)+".vmc.json" for i in range(1,10) \
 for j in range(1,10)]
 
