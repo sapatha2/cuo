70144d4df2d821becb2b03bcb019cd6767b70dad
Shivesh Ananda Pathak <sapatha2@h2ologin1.ncsa.illinois.edu>
2018-08-31 17:03:21 -0500

    biasvariancetradeoff edited for testing

--- qwalk/sampling_test/nchoose10_sgn2/biasvariancetrade.py
+++ qwalk/sampling_test/nchoose10_sgn2/biasvariancetrade.py
@@ -1,26 +1,63 @@
 #Analyze data 
 import numpy as np 
 import sys
-#sys.path.append('/Users/shiveshpathak/Box Sync/Research/Work/si_model_fitting')
+import json 
 sys.path.append('/u/sciteam/sapatha2/si_model_fitting')
+import pandas as pd
 from analyze_jsonlog import compute_and_save,compute_and_save_cut
-import json
+import matplotlib.pyplot as plt
 
-fnames=["Cuvtz0_B3LYP_s3_g0.1.vmc.json"]
-ps=[1]
+'''
+#Identify large err(d<H>/dp) states
+err_cut=0.15 #Error cutoff, include data points with errors bigger than this
+df=pd.read_csv("saved_data.csv")
+new_df=pd.read_csv("saved_data.csv")
+df=df.sort_values(by=["err"],ascending=False)[df['err']>=0.015]
+fnames=df['filename'].values
+ps=df['deriv'].values
+ps=[int(x.split("_")[1]) for x in ps]
 
+#Cutoff analysis 
 for i in range(len(fnames)):
-  cuts=[]
-  vals=[]
-  errs=[]
-  for cut in np.linspace(25.0,5.0,10):
-    for s in range(5): #5 samples of each
-      df=compute_and_save_cut([fnames[i]],cut,ps[i])
-      x=df[df['deriv']=='dpenergy_'+str(ps[i])]
-      print(cut,x['value'],x['err'])
-      cuts.append(cut)
-      vals.append(list(x['value'].values))
-      errs.append(list(x['err'].values))
-  d={'cuts':cuts,'vals':vals,'errs':errs}
-  json.dump(d,open("bvt_"+str(fnames[i])+"_"+str(ps[i])+".json","w"))
-  print(d)
+  dat=(df.iloc[[i]])
+  err=dat['err'].values
+  val=dat['value'].values
+  cut=10.0
+  #Get new estimator
+  while(err>=0.015 and cut>1e-3):
+    cut/=4
+    x=compute_and_save_cut([fnames[i]],cut,ps[i])
+    res=x[x['deriv']==df['deriv'].values[i]]
+    val=res['value'].values[0]
+    err=res['err'].values[0]
+    print(fnames[i],cut,val,err)
+  
+  if(cut>1e-3):
+    #Put back into data frame
+    ind=np.argsort(-new_df['err'])[i]
+    new_df.at[ind,"value"]=val
+    new_df.at[ind,"err"]=err
+    print("##################################################")
+    print(val,err)
+  
+#Write to CSV
+new_df.to_csv("new_saved_data.csv")
+'''
+
+#Plotting
+df=pd.read_csv("saved_data.csv")
+new_df=pd.read_csv("new_saved_data.csv")
+ind=np.argsort(-df['err'])
+ind=ind[df['err']>=0.015].values
+
+new_df=new_df.iloc[ind]
+#for i in range(len(new_df)):
+for i in range(1): 
+  fname=new_df.iloc[i]['filename']
+  deriv=new_df.iloc[i]['deriv']
+  tmp=df[df['filename']==fname]
+  tmp=df[df['deriv']==deriv]
+  f=plt.figure()
+  plt.errorbar(tmp['gsw'],tmp['value'],yerr=tmp['err'])
+  plt.errorbar(new_df.iloc[i]['gsw'],new_df.iloc[i]['value'],yerr=new_df.iloc[i]['err'])
+  f.savefig("tmp.pdf",bbox_inches='tight')
