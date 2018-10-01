8f2c41f9774f8d0dd31ec4165f05d103abb01816
Shivesh Ananda Pathak <sapatha2@h2ologin3.ncsa.illinois.edu>
2018-10-01 09:08:05 -0500

    Working on cutoff test with CuO

--- qwalk/sampling_test/nchoose10_sgn2/analyze.py
+++ qwalk/sampling_test/nchoose10_sgn2/analyze.py
@@ -3,9 +3,26 @@ import numpy as np
 import sys
 #sys.path.append('/Users/shiveshpathak/Box Sync/Research/Work/si_model_fitting')
 sys.path.append('/u/sciteam/sapatha2/si_model_fitting')
-from analyze_jsonlog import compute_and_save
+from analyze_jsonlog import compute_and_save,gather_json_df,bootstrap
 
 fnames=["Cuvtz0_B3LYP_s"+str(i)+"_g0."+str(j)+".vmc.json" for i in range(1,10) \
 for j in range(1,10)]
 
-compute_and_save(fnames,save_name="saved_data.csv")
+for fname in fnames:
+  df=gather_json_df(fname)
+
+  #old way 
+  rsdf=bootstrap(df,100)
+  print(rsdf)
+  print(rsdf.mean())
+  print(rsdf.std())
+
+  #Calculate values
+  for i in range(10):
+    df['dpenergy_'+str(i)]=df['dpwf_'+str(i)]*(df['energy'])
+    df['de_'+str(i)]=df['dpwf_'+str(i)]*(df['energy']-df['energy'].mean()) #new way
+
+  print(df.mean())
+  print(df.std()/np.sqrt(800))
+
+  break
