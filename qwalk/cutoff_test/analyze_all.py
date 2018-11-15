e167cfa03b568782a70d75d1959c53b4b83f9d50
Shivesh Ananda Pathak <sapatha2@h2ologin2.ncsa.illinois.edu>
2018-11-15 12:22:22 -0600

    working on full anlaysis

--- qwalk/cutoff_test/analyze_all.py
+++ qwalk/cutoff_test/analyze_all.py
@@ -44,41 +44,49 @@ plt.savefig('analyze_all1r_e.pdf')
 plt.close()
 '''
 
-#analyze_all2.pdf
-#cutoff=["0","0.25","0.5","0.6","0.65","0.7","0.75","0.775","0.8"]
-#cutoff=['0', '0.0001', '0.00012', '0.00014', '0.00016', '0.00018', '0.0002', '0.00022', '0.00024', '0.00026', '0.00028', '0.0003'] 
+#Figures
 cutoff=['0', '0.0001', '0.00012', '0.00014', '0.00016', '0.00018', '0.0002', '0.00022', '0.00024', '0.00026', '0.00028', '0.0003',
 '0.00035', '0.00045', '0.0005', '0.00055', '0.00065', '0.0007', '0.00075', '0.0008', '0.00085', '0.0009', '0.00095', '0.001']
-count=8
-#for myf in ["/u/sciteam/sapatha2//scratch/nchoose10_sgn2/all"+str(i)+"/Cuvtz0_B3LYP_s3_g0.1_cAll.vmc" for i in range(1,7)]:
-for myf in ["/u/sciteam/sapatha2//scratch/nchoose10_sgn2/all"+str(i)+"/Cuvtz0_B3LYP_s3_g0.1_cAll.vmc" for i in range(8,9)]:
-  count+=1
-  blocki=-1
-  dpwfblocks=[[] for i in range(len(cutoff))]
-  dpeblocks= [[] for i in range(len(cutoff))] 
-  eblocks=   [[] for i in range(len(cutoff))]
-  drop=      [[] for i in range(len(cutoff))]
-  with open(myf+".json") as jsonf:
-    for blockstr in jsonf.read().split("<RS>"):
-      if '{' in blockstr:
-        to_read=blockstr.replace("inf","0")
-        to_read=to_read.replace("},\n}","}\n}")
-        block = json.loads(to_read)['properties']
-        keys=list(block['derivative_dm'].keys())
-        if(len(set(keys).intersection(set(cutoff)))>1): 
-          for i in range(len(cutoff)):
-            dpwfblocks[i].append(block['derivative_dm'][cutoff[i]]['dpwf']['vals'][0])
-            dpeblocks[i].append(block['derivative_dm'][cutoff[i]]['dpenergy']['vals'][0])
-            eblocks[i].append(block['total_energy']['value'][0])
-            drop[i].append(block['derivative_dm'][cutoff[i]]['drop'])
-  dpwfblocks=np.array(dpwfblocks)
-  dpeblocks=np.array(dpeblocks)
-  eblocks=np.array(eblocks)
-  drop=np.array(drop)  
-  for j in range(len(cutoff)):
-    #print(np.mean(dpwfblocks[j]),np.std(dpwfblocks[j])/np.sqrt(len(dpwfblocks[j])))
-    #plt.errorbar(np.mean(drop[j])*100,np.mean(dpwfblocks[j]),yerr=np.std(dpwfblocks[j]/np.sqrt(len(dpwfblocks[j]))),fmt='ob')
-    probplot(dpwfblocks[j],plot=plt)
-    plt.savefig('analyze_all2_'+cutoff[j]+'_'+str(count)+'.png')
+for p in [8,10,11,12]:
+  myf="/u/sciteam/sapatha2/scratch/nchoose10_sgn2/all"+str(p)+"/Cuvtz0_B3LYP_s3_g0.1_cAll.vmc"
+  for parm in range(10): 
+    print(myf,parm)
+    dpwfblocks=[[] for i in range(len(cutoff))]
+    dpeblocks= [[] for i in range(len(cutoff))] 
+    #eblocks=   [[] for i in range(len(cutoff))]
+    drop=      [[] for i in range(len(cutoff))]
+    with open(myf+".json") as jsonf:
+      for blockstr in jsonf.read().split("<RS>"):
+        if '{' in blockstr:
+          to_read=blockstr.replace("inf","0")
+          to_read=to_read.replace("},\n}","}\n}")
+          block = json.loads(to_read)['properties']
+          keys=list(block['derivative_dm'].keys())
+          if(len(set(keys).intersection(set(cutoff)))>1): 
+            for i in range(len(cutoff)):
+              dpwfblocks[i].append(block['derivative_dm'][cutoff[i]]['dpwf']['vals'][parm])
+              dpeblocks[i].append(block['derivative_dm'][cutoff[i]]['dpenergy']['vals'][parm])
+              #eblocks[i].append(block['total_energy']['value'][0])
+              drop[i].append(block['derivative_dm'][cutoff[i]]['drop'])
+    dpwfblocks=np.array(dpwfblocks)
+    dpeblocks=np.array(dpeblocks)
+    #eblocks=np.array(eblocks)
+    drop=np.array(drop)  
+
+    #Figure 1
+    for j in range(len(cutoff)):
+      plt.subplot(211)
+      plt.errorbar(np.mean(drop[j])*100,np.mean(dpwfblocks[j]),yerr=np.std(dpwfblocks[j]/np.sqrt(len(dpwfblocks[j]))),fmt='ob')
+      plt.subplot(212)
+      plt.errorbar(np.mean(drop[j])*100,np.mean(dpeblocks[j]),yerr=np.std(dpeblocks[j]/np.sqrt(len(dpeblocks[j]))),fmt='og')
+    plt.savefig('fig/analyze_all1_'+str(p)+'_'+str(parm)+'.pdf') 
     plt.close()
-  #plt.show() 
+
+    #Figure 2
+    for j in range(len(cutoff)):
+      probplot(dpwfblocks[j],plot=plt)
+      plt.savefig('fig/analyze_all2_'+cutoff[j]+'_'+str(p)+'_'+str(parm)+'.png')
+      plt.close()
+      probplot(dpeblocks[j],plot=plt)
+      plt.savefig('fig/analyze_all2_'+cutoff[j]+'_'+str(p)+'_'+str(parm)+'e.png')
+      plt.close()
