f99f71586752939a652bbdca39ac9e6440c91f20
Shivesh Ananda Pathak <sapatha2@h2ologin3.ncsa.illinois.edu>
2018-12-20 11:38:49 -0600

    working on input file generatino for vmc

---
+++ qwalk/vmc/geninput.py
@@ -0,0 +1,63 @@
+#Generate input files 
+import os 
+import shutil 
+import numpy as np 
+
+def geninput(detgen,N,Ndet,gsw,basename):
+  '''
+  input:
+  detgen - type of excitations to sample
+  N - number of states to sample per spin (2*N states in total)
+  Ndet - number of determinants per state
+  gsw - ground state weight 
+  basename - all written files to basename/__generated file names__
+  output:
+  slater files
+  vmc files 
+  pbs files for vmc
+  '''
+  
+  #Make sure directory exists, else make it 
+  if not os.path.exists(basename):
+    #Move all important files to there 
+    shutil.copytree('req_files',basename+'/') 
+  else:
+    print('Directory '+str(basename)+' exists, not going to overwrite')
+    exit(0)  
+
+  fout='_'+detgen+'_Ndet'+str(Ndet)+'_gsw'+str(np.round(gsw,2))
+  genslater(N,Ndet,gsw,basename,fout)
+  genvmc(N,basename,fout)
+  genpbs(N,basename,fout)
+  return 
+
+def genpbs(N,basename,fout):
+  for state in ['','2']:
+    for j in range(1,N+1):
+      fname='ex'+state+fout+'_'+str(j)
+      
+      string='#!/bin/bash\n'+\
+      '#PBS -q low\n'+\
+      '#PBS -l nodes=2:ppn=32:xe\n'+\
+      '#PBS -l walltime=02:00:00\n'+\
+      '#PBS -N '+fname+'\n'\
+      '#PBS -e '+fname+'.perr\n'+\
+      '#PBS -o '+fname+'.pout\n'+\
+      'mkdir -p /scratch/sciteam/$USER/cuo/qwalk/vmc/'+str(basename)+'\n'+\
+      'cd /scratch/sciteam/$USER/cuo/qwalk/vmc/'+str(basename)+'\n'+\
+      'cp -u /u/sciteam/$USER/cuo/qwalk/vmc/'+str(basename)+'/* .\n'+\
+      'aprun -n 64 /u/sciteam/$USER/fork/bin/qwalk '+fname+'.vmc &> '+fname+'.vmc.out\n'
+      
+      f=open(basename+'/'+fname+'.pbs','w')
+      f.write(string)
+      f.close()      
+  return 1
+
+def genvmc(N,basename,fout):
+  return -1
+
+def genslater(N,Ndet,gsw,basename,fout):
+  return -1 
+
+if __name__=='__main__':
+  geninput('s',100,10,0.7,basename='run1/')
