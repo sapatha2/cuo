9125a1edc1c55d18477fc5d4132d76b9f95c33ca
Shivesh Pathak <sapatha2@illinois.edu>
2018-12-20 16:54:42 -0800

    reduce DF code written, now to analyze!

--- qwalk/vmc/run1s/analyze.py
+++ qwalk/vmc/run1s/analyze.py
@@ -1,14 +1,8 @@
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.api as sm 
+import pandas as pd
 
-df=pd.read_pickle('s_Ndet10_gsw0.7_df.pickle')
-print(df.shape)
-
-#Energy check
-e=df['energy']*27.2114 #eV
-e-=np.min(e)
-plt.hist(e,bins=20)
-plt.show()
-
+fname='s_Ndet10_gsw0.7_df.pickleR'
+df=pd.read_pickle(fname)
 
