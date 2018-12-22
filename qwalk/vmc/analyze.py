6c0fa1eed39f42bdee452a6a51594bb20be82c79
Shivesh Pathak <sapatha2@illinois.edu>
2018-12-21 23:21:57 -0800

    .

--- qwalk/vmc/analyze.py
+++ qwalk/vmc/analyze.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm 
 import pandas as pd
 
-#flist=['run1s/s_Ndet10_gsw0.7_df.pickleR','run2s/s_Ndet10_gsw0.7_df.pickleR']
-flist=['run2a/a_Ndet10_gsw0.8_df.pickleR','run3a/a_Ndet10_gsw0.8_df.pickleR']
+flist=['run1s/s_Ndet10_gsw0.7_df.pickleR','run2s/s_Ndet10_gsw0.7_df.pickleR']
+#flist=['run2a/a_Ndet10_gsw0.8_df.pickleR','run3a/a_Ndet10_gsw0.8_df.pickleR']
 df=None
 for fname in flist:
   small=pd.read_pickle(fname)
@@ -14,7 +14,7 @@ for fname in flist:
   else: df=pd.concat((df,small))
 
 y=df['energy']
-X=df.drop(columns=['energy','energy_err','gsw','detgen','obdm_3dz2_3dz2'])
+X=df.drop(columns=['energy','energy_err','gsw','detgen','obdm_4s_3dz2','obdm_3dz2_3dz2','obdm_3dpi_3dpi'])
 X=sm.add_constant(X)
 ols=sm.OLS(y,X).fit()
 print(ols.summary())
@@ -23,6 +23,6 @@ plt.ylabel('E_VMC (eV)')
 plt.xlabel('E_Pred (eV)')
 plt.errorbar(ols.predict(X),y,yerr=df['energy_err'],fmt='bo')
 plt.plot(y,y,'g--')
-plt.title('Full active space 1-body fit')
-plt.savefig('a_Ndet10_gsw0.8_df.pdf')
-plt.show()
+plt.title('Singles space 1-body fit')
+plt.savefig('s_Ndet10_gsw0.7_df.pdf')
+#plt.show()
