7e232b7c581d462fe9242c7b9cd7f8ca27ff8007
Shivesh Pathak <sapatha2@illinois.edu>
2019-01-16 10:39:59 -0600

    working on equilibrium CuO calculations. Singles, All, LCS, LCA setup for now

---
+++ qwalk/vmc_eq/subpbs.py
@@ -0,0 +1,16 @@
+import os 
+import numpy as np 
+
+def subpbs(detgen,N,Ndet,gsw,basename):
+  fout='_'+detgen+'_Ndet'+str(Ndet)+'_gsw'+str(np.round(gsw,2))
+  for state in ['gs0','gs1','gs2','gs3','gs4','gs5','gs2_lo','gs3_lo','gs5_lo']:
+    for j in range(1,N+1):
+      fname=basename+'/'+state+fout+'_'+str(j)+'.pbs'
+      os.system('qsub '+fname)
+
+if __name__=='__main__':
+  detgen='s'
+  N=100
+  Ndet=2
+  gsw=0.0
+  subpbs(detgen,N,Ndet,gsw,basename='run1s/')
