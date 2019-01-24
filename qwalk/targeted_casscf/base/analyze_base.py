6cc8508d9197bc9c782500ecac099f3b5fa3fc13
Shivesh Pathak <sapatha2@illinois.edu>
2019-01-24 13:07:17 -0600

    added delta files

--- qwalk/targeted_casscf/base/analyze_base.py
+++ qwalk/targeted_casscf/base/analyze_base.py
@@ -3,7 +3,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df=pd.read_pickle('base_gosling.pickle')
-df['E']-=min(df['E'])
-df['E']*=27.2114
-df=df.sort_by(column=['E'])
-print(df[['E','n_3d','n_4s','n_2ppi','n_2pz','J_4s_3d']])
+df['energy']-=min(df['energy'])
+df=df.sort_values(by=['energy'])
+print(df[['base_state','energy','n_3d','n_4s','n_2ppi','n_2pz','J_4s_3d']])
