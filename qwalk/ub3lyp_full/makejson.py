5a040de4cca38f073761fcc88d3c95a01faf5ec2
Shivesh Pathak <sapatha2@illinois.edu>
2019-02-01 10:07:36 -0600

    gathered 1.0 - 0.6 gsw

--- qwalk/ub3lyp_full/makejson.py
+++ qwalk/ub3lyp_full/makejson.py
@@ -4,15 +4,18 @@ import numpy as np
 def makejson(N,gsw,basename):
   for j in range(1,N+1):
     f=basename+'/gsw'+str(np.around(gsw,2))+'_'+str(j)+'.vmc'
-    print(f)
+    #if(os.path.isfile(f+'.gosling.json')): print(f+'.gosling.json exists') 
+    #if("pee"): pass
+    #else:
+    print('Running '+f+'.gosling.json')
     os.system('../../../mainline/bin/gosling '+f+'.log -json &> '+f+'.gosling.json')
   return 1
 
 if __name__=='__main__':
   N=10
   for basestate in np.arange(16):
-    #for gsw in np.arange(0.1,1.1,0.1):
-    for gsw in [1.0]:  
+    for gsw in np.arange(0.6,1.0,0.1):
+    #for gsw in [1.0]:  
       if(gsw==1.0): N=1
       else: N=10
       makejson(N,gsw,basename='gsw'+str(np.around(gsw,2))+'b'+str(basestate))
