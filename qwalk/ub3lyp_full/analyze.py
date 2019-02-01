8c10781ed035a879d3a6e0ae8049955bb2ff04e9
Shivesh Pathak <sapatha2@illinois.edu>
2019-02-01 10:18:45 -0600

    working on analysis file

--- qwalk/ub3lyp_full/analyze.py
+++ qwalk/ub3lyp_full/analyze.py
@@ -10,12 +10,12 @@ from sklearn.model_selection import cross_val_score
 def collectdf():
   df=None
   for basestate in range(16):
-    #for gsw in np.arange(0.1,1.1,0.1):
-    for gsw in [1.0]:  
+    for gsw in np.arange(0.6,1.01,0.1):
       f='gsw'+str(np.round(gsw,2))+'b'+str(basestate)+'/gosling.pickle' 
       small_df=pd.read_pickle(f)
       
       small_df['basestate']=basestate
+      if(np.around(gsw,2)==1.0): small_df['basestate']=-1
       if(df is None): df = small_df
       else: df = pd.concat((df,small_df),axis=0)
   return df
@@ -23,42 +23,11 @@ def collectdf():
 def analyze(df):
   df['gsw']=np.round(df['gsw'],2)
   #PAIRPLOTS --------------------------------------------------------------------------
-  #Full
-  df=df[(df['basestate']==0)+(df['basestate']==2)+(df['basestate']==3)+ 
-  (df['basestate']==5)+(df['basestate']==6)+(df['basestate']==7)+(df['basestate']==8)
-  +(df['basestate']==9)]
-  sns.pairplot(df,vars=['energy','n_3d','n_2ppi','n_2pz','U_4s','J_4s_3d'],hue='basestate')
-  #df=df[(df['basestate']==7)+(df['basestate']==-1)]
-  #sns.pairplot(df,vars=['energy','n_3d','n_2ppi','n_2pz'],hue='basestate',markers=['o']+['.']*1)
+  sns.pairplot(df,vars=['energy','n_3d','n_2ppi','n_2pz','U_4s','J_4s_3d'],hue='basestate',markers=['o']+['.']*16)
   plt.show()
   exit(0)
-  #plt.savefig('all_base.pdf',bbox_inches='tight')
 
-  #Each basestate
-  #for b in range(8):
-  #  dat=df[(df['basestate']==b)+(df['basestate']==-1)]
-  #  sns.pairplot(dat,vars=['energy','n_3d','J_4s_3d'],hue='gsw',markers=['o']+['.']*9)
-  #  plt.savefig(str(b)+'_base.pdf',bbox_inches='tight')
-  #  plt.close()
-  
-  #LINREG ---------------------------------------------------------------------------
-  #Full
-  #df=df[(df['basestate']==0)+(df['basestate']==-1)]
-  '''
-  y=df['energy']
-  yerr=df['energy_err']
-  X=df[['n_3d','n_2ppi','n_2pz','n_4s','3dz2-2pz','4s-3dz2','4s-2pz']]
-  reg=OrthogonalMatchingPursuitCV(cv=5).fit(X,y)
-  print(reg.score(X,y))
-  print(reg.n_nonzero_coefs_,reg.n_iter_)
-  print(reg.intercept_)
-  print(reg.coef_[reg.coef_!=0])
-  print(np.array(list(X))[reg.coef_!=0])
-  #df['pred']=reg.predict(X)
-  #sns.pairplot(df,vars=['energy','pred'],hue='basestate',markers=['o']+['.']*8)
-  #sns.pairplot(df,vars=['energy','pred'],hue='gsw',markers=['o']+['.']*9)
-  #plt.savefig('all_fit.pdf',bbox_inches='tight')
-  '''
+  #FITS --------------------------------------------------------------------------
   y=df['energy']
   yerr=df['energy_err']
   X=df[['n_3d','n_2ppi','n_2pz','3dz2-2pz','4s-2pz','3dpi-2ppi']]
@@ -70,23 +39,6 @@ def analyze(df):
     df['pred']=wls.predict(X)
     sns.pairplot(df,vars=['energy','pred'],hue='basestate',markers=['o']+['.']*9)
     plt.show()
-
-  #GROUND STATE ONLY
-  '''
-  0.9537834073367943
-  5 5
-  -5768.597210709244
-  [-3.39486121 -2.06520991  1.21081355  1.88308435  1.62724933]
-  ['n_3d' 'n_2pz' '3dpi-2ppi' '3dz2-2pz' '4s-2pz']
-  '''
-  #ALL SAMPLES
-  '''
-  0.9594805237781355
-  5 5
-  -5768.280708641785
-  [-3.38004168 -2.25961628  1.19411177  2.19104091  1.75083078]
-  ['n_3d' 'n_2pz' '3dpi-2ppi' '3dz2-2pz' '4s-2pz']
-  '''
 
 if __name__=='__main__':
   df=collectdf()
