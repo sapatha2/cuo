76897311dfc1643e4015dcc9549cd76d2ed3ff64
Shivesh Pathak <sapatha2@illinois.edu>
2019-02-24 15:29:31 -0600

    finished analysis for VMC, going to run new_gs, see if its any good

--- qwalk/ub3lyp_mo3/analyze.py
+++ qwalk/ub3lyp_mo3/analyze.py
@@ -4,15 +4,14 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
-from sklearn.linear_model import OrthogonalMatchingPursuit, OrthogonalMatchingPursuitCV 
+from sklearn import linear_model
 from sklearn.model_selection import cross_val_score
-from pyscf import lib,gto,scf,mcscf,fci,lo,ci,cc
-from pyscf.scf import ROHF,UHF,ROKS
-from functools import reduce 
+from sklearn.metrics import mean_squared_error, r2_score
+from diagonalize import diagonalize, new_gs
 
 def collectdf():
   df=None
-  for basestate in range(5):
+  for basestate in range(10):
     for gsw in np.arange(0.1,1.01,0.1):
       f='gsw'+str(np.round(gsw,2))+'b'+str(basestate)+'/gosling.pickle' 
       small_df=pd.read_pickle(f)
@@ -24,8 +23,8 @@ def collectdf():
   return df
 
 def analyze(df):
+  #Formatting
   df['gsw']=np.round(df['gsw'],2)
-  
   df['n_3dd']=df['t_8_8']+df['t_9_9']
   df['n_3dpi']=df['t_5_5']+df['t_6_6']
   df['n_3dz2']=df['t_7_7']
@@ -40,67 +39,74 @@ def analyze(df):
   df['t_sz']=2*df['t_12_13']
 
   #PAIRPLOTS --------------------------------------------------------------------------
-  #sns.pairplot(df,vars=['energy','n_3dd','n_3dpi','n_3dz2','n_3d'],hue='basestate',markers=['o']+['.']*8)
-  #sns.pairplot(df,vars=['energy','n_2ppi','n_2pz','n_2p','n_4s'],hue='basestate',markers=['o']+['.']*8)
-  #sns.pairplot(df,vars=['energy','t_pi','t_dz','t_ds','t_sz'],hue='basestate',markers=['o']+['.']*8)
+  #sns.pairplot(df,vars=['energy','n_3dd','n_3dpi','n_3dz2','n_3d'],hue='basestate',markers=['o']+['.']*10)
+  #sns.pairplot(df,vars=['energy','n_2ppi','n_2pz','n_2p','n_4s'],hue='basestate',markers=['o']+['.']*10)
+  #sns.pairplot(df,vars=['energy','t_pi','t_dz','t_ds','t_sz'],hue='basestate',markers=['o']+['.']*10)
   #plt.show()
   #exit(0)
 
-  #FITS --------------------------------------------------------------------------
-  y=df['energy']
-  yerr=df['energy_err']
-  #BIGGEST
-  #X=df[['n_3dd','n_3d','n_2ppi','n_2pz','n_2p','n_4s','t_pi','t_sz','t_dz','t_ds']]
-  #SMALLEST
-  #X=df[['n_3d','n_2ppi','n_2pz']]
-  #SEQUENTIALLY BETTER
-  #X=df[['n_3d','n_2ppi','n_2pz','t_pi']]
-  #X=df[['n_3d','n_2ppi','n_2pz','t_ds']]
-  X=df[['n_3d','n_2ppi','n_2pz','t_pi','t_dz','t_sz']]
+  #R2, RMSE AND MODEL PLOTS ----------------------------------------------------------
+  zz=0
+  ncv=5
+  model_list=[
+    ['n_3d','n_2ppi','n_2pz'],
+    ['n_3d','n_2ppi','n_2pz','t_pi'],
+    ['n_3d','n_2ppi','n_2pz','t_ds'],
+    ['n_3d','n_2ppi','n_2pz','t_pi','t_ds'],
+    ['n_3d','n_2ppi','n_2pz','t_pi','t_ds','t_dz'],
+    ['n_3d','n_2ppi','n_2pz','t_pi','t_ds','t_sz'],
+    ['n_3d','n_2ppi','n_2pz','t_pi','t_ds','t_dz','t_sz']
+  ]
+  for model in model_list:
+    y=df['energy']
+    X=df[model]
+    X=sm.add_constant(X)
+    ols=linear_model.LinearRegression().fit(X,y)
+    
+    full_r2=r2_score(y,ols.predict(X))
+    full_rmse=mean_squared_error(y,ols.predict(X))
+    cv_r2=cross_val_score(ols,X,y,cv=ncv,scoring='r2')
+    cv_rmse=cross_val_score(ols,X,y,cv=ncv,scoring='neg_mean_squared_error')
+    
+    coefs=ols.coef_[1:]
+    parms=[0]
+    zzz=0
+    for i in range(len(model_list[-1])):
+      if(model_list[-1][i] in model):
+        parms.append(coefs[zzz])
+        zzz+=1
+      else: parms.append(0)
+    
+    evals=diagonalize(parms)
+    if(zz==3): new_gs(parms) 
+    
+    plt.plot(np.ones(ncv)*zz,cv_r2,'go-')
+    plt.plot(np.ones(ncv)*zz+0.15,cv_rmse,'bo-')
+    
+    if(zz==0):
+      plt.plot([zz],full_r2,'gs',label='R2')
+      plt.plot([zz+0.15],full_rmse,'bs',label='RMSE')
+      plt.plot(np.ones(len(evals))*zz+0.30,evals/10,'ro-',label='Eval (eV)/10')
+    else:
+      plt.plot([zz],full_r2,'gs')
+      plt.plot([zz+0.15],full_rmse,'bs')
+      plt.plot(np.ones(len(evals))*zz+0.30,evals/10,'ro-')
 
-  X=sm.add_constant(X)
-  ols=sm.OLS(y,X).fit()
-  print(ols.params)
-  print(ols.summary())
-  
+    zz+=1
+  #plt.legend(loc='best')
+  #plt.xlabel('Model')
+  #plt.savefig('model_valid.pdf',bbox_inches='tight')
+  #exit(0)
+
+  #PREDICTION PLOTS ---------------------------------------------------------------
+  '''
   df['pred']=ols.predict(X)
-  g = sns.FacetGrid(df,hue='basestate',hue_kws=dict(marker=['o']+['.']*8))
+  g = sns.FacetGrid(df,hue='basestate',hue_kws=dict(marker=['o']+['.']*10))
   g.map(plt.errorbar, "pred", "energy", "energy_err",fmt='o').add_legend()
   plt.plot(df['energy'],df['energy'],'k--')
   plt.show()
   exit(0)
-
-  #ROTATE TO IAOS --------------------------------------------------------------------------
-  #Gather IAOs
-  f='../../pyscf/analyze/b3lyp_iao_b.pickle' #IAOs which span the MOs
-  a=np.load(f)
-
-  chkfile='../../pyscf/chk/Cuvtz_r1.725_s1_B3LYP_1.chk' #MOs we used to calculate RDM elements
-  mol=lib.chkfile.load_mol(chkfile)
-  m=ROKS(mol)
-  m.__dict__.update(lib.chkfile.load(chkfile, 'scf'))
-
-  s=m.get_ovlp()
-  H1=np.diag([-3.1957,-3.1957,-3.1957,-3.1957,-3.1957,-1.6972,-1.6972,-2.5474,0])
-  H1[0,5]=0.7858
-  H1[5,0]=0.7858
-  H1[1,6]=0.7858
-  H1[6,1]=0.7858
-  H1[2,7]=-0.8329
-  H1[7,2]=-0.8329
-  H1[7,8]=1.6742
-  H1[8,7]=1.6742
-
-  mo_coeff=m.mo_coeff[:,5:14]
-  e1=reduce(np.dot,(a.T,s,mo_coeff,H1,mo_coeff.T,s.T,a))
-  e1=(e1+e1.T)/2. 
-  
-  plt.matshow(e1,vmax=5,vmin=-5,cmap=plt.cm.bwr)
-  plt.colorbar()
-  labels=['3s','4s','3px','3py','3pz','3dxy','3dyz','3dz2','3dxz','3dx2-y2','2s','2px','2py','2pz']
-  plt.xticks(np.arange(14),labels,rotation=90)
-  plt.yticks(np.arange(14),labels)
-  plt.show()
+  '''
 
 if __name__=='__main__':
   df=collectdf()
