bce6e90abaecb9ca6cd244da5ac19369c2416379
Shivesh Pathak <sapatha2@illinois.edu>
2019-03-03 14:41:56 -0600

    finished so, do with 2rdm

--- qwalk/ub3lyp_do_s1/gather.py
+++ qwalk/ub3lyp_do_s1/gather.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys
 sys.path.append('../../../downfolding/')
 from shivesh_downfold_tools import get_qwalk_dm, sum_onebody, sum_J, sum_U, sum_V
+import os 
 
 full_labels=np.array(["3s","3pz","3py","3px","2pz","3dz2","3dxz","3dyz"])
 def gather_all(N,gsw,basename):
@@ -12,13 +13,14 @@ def gather_all(N,gsw,basename):
   '''
   df=None
   for j in range(1,N+1):
-    if((gsw==0.6) and (j==5)): pass
-    else:
-      f=basename+'/gsw'+str(np.round(gsw,2))+'_'+str(j)+'.vmc.gosling.json' 
-      print(f)
+    f=basename+'/gsw'+str(np.round(gsw,2))+'_'+str(j)+'.vmc.gosling.json' 
 
+    statinfo=os.stat(f)
+    if(statinfo.st_size>0):
+      print(f)
       data=json.load(open(f,'r'))
-      obdm,__=get_qwalk_dm(data['properties']['tbdm_basis'])
+      obdm,__=get_qwalk_dm(data['properties']['tbdm_basis1'])
+      __,__,tbdm,__=get_qwalk_dm(data['properties']['tbdm_basis2'])
       energy=data['properties']['total_energy']['value'][0]*27.2114
       energy_err=data['properties']['total_energy']['error'][0]*27.2114
 
@@ -27,14 +29,17 @@ def gather_all(N,gsw,basename):
       one_body=sum_onebody(obdm,orb1,orb2)
       one_labels=['t_'+str(orb1[i])+'_'+str(orb2[i]) for i in range(len(orb1))]
 
-      dat=np.array([energy,energy_err]+list(one_body))
-      d=pd.DataFrame(dat[:,np.newaxis].T,columns=['energy','energy_err']+one_labels)
+      sigU=0.5*(data['properties']['tbdm_basis2']['tbdm']['updown'][0]+\
+      data['properties']['tbdm_basis2']['tbdm']['downup'][0])
+
+      dat=np.array([energy,energy_err,sigU]+list(one_body))
+      d=pd.DataFrame(dat[:,np.newaxis].T,columns=['energy','energy_err','sigU']+one_labels)
       d=d.astype('double')
       d['gsw']=gsw
       if(j>N): d['gsw']=0
       if(df is None): df=d
       else: df=pd.concat((df,d),axis=0)      
-
+    else: print(f+' does not exist')
   fout=basename+'/gosling.pickle'
   df.to_pickle(fout)
   return df
--- qwalk/ub3lyp_do_s1/makejson.py
+++ qwalk/ub3lyp_do_s1/makejson.py
@@ -6,6 +6,16 @@ def makejson(N,gsw,basename):
     f=basename+'/gsw'+str(np.around(gsw,2))+'_'+str(j)+'.vmc'
     print(f)
     os.system('../../../mainline/bin/gosling '+f+'.log -json &> '+f+'.gosling.json')
+  
+    #Replace labels in JSON 
+    json_f=open(f+'.gosling.json','r').read().split("\n")
+    i=1 
+    for j in range(len(json_f)):
+      if('tbdm_basis' in json_f[j]): 
+        json_f[j]='"tbdm_basis'+str(i)+'":{'
+        i+=1
+    json_o=open(f+'.gosling.json','w')
+    json_o.write('\n'.join(json_f))
   return 1
 
 if __name__=='__main__':
