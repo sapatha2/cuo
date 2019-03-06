c2261bad6f59790afe378f3451cf0e70e93a7e61
Shivesh Ananda Pathak <sapatha2@h2ologin3.ncsa.illinois.edu>
2019-03-06 12:11:13 -0600

    gather.

--- qwalk/ub3lyp_base_s1/gather.py
+++ qwalk/ub3lyp_base_s1/gather.py
@@ -41,12 +41,12 @@ def gather_all(N,gsw,basename):
       iao_labels=['iao_'+str(orb1[i])+'_'+str(orb2[i]) for i in range(len(orb1))]
     
       #2-body
-      orb1=[0,1,2,3,4,5]
+      orb1=[0,1,2,3,4,5,6,7,8]
       u=sum_U(tbdm,orb1)
       u_labels=['u'+str(orb1[i]) for i in range(len(orb1))]
 
-      orb1=[0,0,0,0,0,1,1,1,1,2,2,2,3,3,4]
-      orb2=[1,2,3,4,5,2,3,4,5,3,4,5,4,5,5]
+      orb1=[0,0,0,0,0,1,1,1,1,2,2,2,3,3,4,0,0,0]
+      orb2=[1,2,3,4,5,2,3,4,5,3,4,5,4,5,5,6,7,8]
       j=sum_J(tbdm,orb1,orb2)
       j_labels=['j_'+str(orb1[i])+'_'+str(orb2[i]) for i in range(len(orb1))]
 
