5b7d799cb03eae9f904ca210f29c3a2a6ca8ccce
Shivesh Pathak <sapatha2@illinois.edu>
2019-03-15 09:54:50 -0500

    finished beta fitting and ED

--- qwalk/ub3lyp_s1_/analyzedmc.py
+++ qwalk/ub3lyp_s1_/analyzedmc.py
@@ -11,6 +11,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 from sklearn.model_selection import KFold
 from scipy import stats
+from ed import ED
 
 ######################################################################################
 #FROZEN METHODS
@@ -69,35 +70,39 @@ def format_df(df):
   return df
 
 #Single parameter goodness of fit validation
-def oneparm_valid(df,ncv,model_list,save=False):
+def oneparm_valid(df,ncv,model_list,save=False,weights=None):
   zz=0
   kf=KFold(n_splits=ncv,shuffle=True)
+  if(weights is None): weights=np.ones(df.shape[0])
+  df['weights']=weights
   for model in model_list:
     y=df['energy']
     X=df[model]
     X=sm.add_constant(X)
-
+   
+    #10 samples of ncv
     r2_train=[]
     r2_test=[]
     r2=[]
     evals=[]
-    for train_index,test_index in kf.split(df):
-      X_train,X_test=X.iloc[train_index],X.iloc[test_index]
-      y_train,y_test=y.iloc[train_index],y.iloc[test_index]
-
-      ols=linear_model.LinearRegression().fit(X_train,y_train)
-      r2_train.append(r2_score(y_train,ols.predict(X_train)))
-      r2_test.append(r2_score(y_test,ols.predict(X_test)))
-      r2.append(r2_score(y,ols.predict(X)))
+    for i in range(10):
+      for train_index,test_index in kf.split(df):
+        X_train,X_test=X.iloc[train_index],X.iloc[test_index]
+        y_train,y_test=y.iloc[train_index],y.iloc[test_index]
+        w_train,w_test=df['weights'].iloc[train_index],df['weights'].iloc[test_index]
+        ols=linear_model.LinearRegression().fit(X_train,y_train,w_train)
+        r2_train.append(r2_score(y_train,ols.predict(X_train),w_train))
+        r2_test.append(r2_score(y_test,ols.predict(X_test),w_test))
+        r2.append(r2_score(y,ols.predict(X),weights))
     
     if(zz==0):
-      plt.plot(np.ones(ncv)*zz,r2_test,'gs-',label='r2 test')
-      plt.plot(np.ones(ncv)*zz+0.10,r2_train,'bo-',label='r2 train')
-      plt.plot(np.ones(ncv)*zz+0.20,r2,'r*-',label='full r2')
+      plt.plot(np.ones(ncv*(i+1))*zz,r2_test,'gs-',label='r2 test')
+      plt.plot(np.ones(ncv*(i+1))*zz+0.10,r2_train,'bo-',label='r2 train')
+      plt.plot(np.ones(ncv*(i+1))*zz+0.20,r2,'r*-',label='full r2')
     else:
-      plt.plot(np.ones(ncv)*zz,r2_test,'gs-')
-      plt.plot(np.ones(ncv)*zz+0.10,r2_train,'bo-')
-      plt.plot(np.ones(ncv)*zz+0.20,r2,'r*-')
+      plt.plot(np.ones(ncv*(i+1))*zz,r2_test,'gs-')
+      plt.plot(np.ones(ncv*(i+1))*zz+0.10,r2_train,'bo-')
+      plt.plot(np.ones(ncv*(i+1))*zz+0.20,r2,'r*-')
     zz+=1
   plt.legend(loc='best')
   plt.title('R2 CV DMC')
@@ -171,16 +176,7 @@ def regr_plot(df,model,weights=None,save=False):
   df['pred']=ols.predict(Z)
   df['resid']=df['energy']-df['pred']
   df['pred_err']=(u_ols-l_ols)/2
-
   '''
-  sns.pairplot(df,vars=['energy','pred']+model,hue='Sz')
-  if(save):
-    plt.savefig('analysis/fit_pairplot.pdf',bbox_inches='tight')
-  else:
-    plt.show()
-  plt.close()
-  '''
-
   g = sns.FacetGrid(df,hue='Sz',hue_kws=dict(marker=['.']*3))#,hue='basestate',hue_kws=dict(marker=['o']+['.']*16))
   g.map(plt.errorbar, "pred", "energy", "energy_err","pred_err",fmt='o').add_legend()
   plt.plot(df['energy'],df['energy'],'k--')
@@ -199,111 +195,145 @@ def regr_plot(df,model,weights=None,save=False):
   else:
     plt.show()
   plt.close()
+  '''
+  return ols
+
+#Weighted bootstrap sampling
+def bootstrap(df,n,model,weights=None,save=False):
+  if(weights is None): bootstrap=np.ones(df.shape[0])
+  rdf = df.sample(n=n,weights=weights,replace=True) #Resampled df, using weights, with replacement
+  regr_plot(rdf,model,save=save)
+  
   return 1
 
 ######################################################################################
 #Analysis pipeline, main thing to edit for runs
 def analyze(df,save=False):
+  ncv=5
+
   '''
-  ncv=10
   model_list=[
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_ds'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds'],
    
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','Us'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','Us'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_ds','Us'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds','Us'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz','Us'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi','Us'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds','Us'],
  
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','Jsd'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','Jsd'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_ds','Jsd'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds','Jsd'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz','Jsd'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi','Jsd'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds','Jsd'],
   
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','Jsd','Us'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','Jsd','Us'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_ds','Jsd','Us'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds','Jsd','Us'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz','Jsd','Us'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi','Jsd','Us'],
+    ['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds','Jsd','Us'],
   ]
   oneparm_valid(df,ncv,model_list,save=save)
+  '''
   
-  model_list=[
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds'],
-    
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','Us'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds','Us'],
-    
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','Jsd'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds','Jsd'],
-  
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','Jsd','Us'],
-    ['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','mo_t_ds','Jsd','Us'],
-  ]
-  resid_valid(df,model_list,save=save)
+  '''
+  model=['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi','Us']
+  y=df['energy']
+  X=df[model]
+  X=sm.add_constant(X)
+  df['resid']=df['energy']-sm.OLS(y,X).fit().predict()
+  df['pred']=sm.OLS(y,X).fit().predict()
+  sns.pairplot(df,vars=['energy','pred','resid','Jsd'],hue='Sz')
+  plt.show()
+  exit(0)
   '''
 
-  model=['mo_n_3d','mo_n_2ppi','mo_n_2pz','mo_t_pi','Jsd','Us','Ud']
-  
+  #model=['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi','Us']
+  #regr_plot(df,model)
+  #exit(0)
+
+  '''
+  model=['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi','Jcu','Us']
   #Logistical sigmoid with 1/2 cutoff specified
   def cut_sigmoid(y,cutoff,beta):
     return np.exp(-beta*(y-cutoff))/(np.exp(-beta*(y-cutoff))+1)
 
-  '''
-  cut=0.5
-  beta=7
   zz=0
-
-  kf=KFold(n_splits=5,shuffle=True)
-  for cut in [0.7,0.6,0.5,0.4,0.3]:
-    for beta in [3,5,7,9]:
-      
-      weights=cut_sigmoid(-df['mo_n_3d']+max(df['mo_n_3d']),cut,beta)
-      y=df['energy']
-      X=df[model]
-      X=sm.add_constant(X)
-
-      r2_train=[]
-      r2_test=[]
-      r2=[]
-      evals=[]
+  ncv=10
+  Nrun=1 #How many times to sample CV DISTRIBUTION 
+  kf=KFold(n_splits=ncv,shuffle=True)
+  for beta in np.arange(0,3.5,0.5):
+    #weights=cut_sigmoid(-df['mo_n_3d']+max(df['mo_n_3d']),cut,beta)
+    print(beta,"-----------------")
+    y=df['energy']
+    X=df[model]
+    X=sm.add_constant(X)
+    df['weights']=np.exp(-beta*(y-min(y)))
+    weights=df['weights']
+    
+    r2_train=[]
+    r2_test=[]
+    r2=[]
+    evals=[]
+    for n in range(Nrun):
       for train_index,test_index in kf.split(df):
         X_train,X_test=X.iloc[train_index],X.iloc[test_index]
         y_train,y_test=y.iloc[train_index],y.iloc[test_index]
-        print(train_index.shape)
-        train_weights=weights[np.array(train_index)]
-        test_weights= weights[np.array(test_index)]
+        train_weights=weights.iloc[train_index]
+        test_weights= weights.iloc[test_index]
 
         ols=linear_model.LinearRegression().fit(X_train,y_train,train_weights)
         r2_train.append(r2_score(y_train,ols.predict(X_train),train_weights))
         r2_test.append(r2_score(y_test,ols.predict(X_test),test_weights))
         r2.append(r2_score(y,ols.predict(X),weights))
-    
-    plt.plot(np.ones(ncv)*zz,    r2_train,'gs')
-    plt.plot(np.ones(ncv)*zz+0.1,r2_test,'bo')
-    plt.plot(np.ones(ncv)*zz+0.2,r2,'r*')
+   
+    plt.plot(np.ones(ncv*Nrun)*beta,    r2_train,'g.')
+    plt.plot(np.ones(ncv*Nrun)*beta+0.1,r2_test,'b.')
+    plt.plot(np.ones(ncv*Nrun)*beta+0.2,r2,'r*')
     zz+=1
+  plt.title("cv, model="+' '.join(model))
   plt.show()
-  exit(0)
   '''
 
-  #plt.plot(df['mo_n_3d'],weights,'.')
-  #plt.xlabel('mo_n_3d')
-  #plt.ylabel('weight')
-  #plt.title('cut='+str(cut)+', beta='+str(beta))
-  #plt.savefig('analysis/weight.pdf')
-  weights=None
-  regr_plot(df,model,weights,save=save)
+  full_df=None
+  model=['mo_n_4s','mo_n_2ppi','mo_n_2pz','mo_t_pi','Jsd','Us']
+  for beta in np.arange(0,3.75,0.25):
+    weights=np.exp(-beta*(df['energy']-min(df['energy'])))
+    fit=regr_plot(df,model,weights,save)
+    params=list(fit.params[1:5])+[0,0,0]+list(fit.params[5:])
+    norb=9
+
+    nelec=(8,7)
+    nroots=14
+    res1=ED(params,nroots,norb,nelec)
+
+    nelec=(9,6)
+    nroots=6
+    res3=ED(params,nroots,norb,nelec)
+
+    E = res1[0]
+    n_occ = res1[2]+res1[3]
+    Sz = np.ones(len(E))*0.5
+    n_3d = n_occ[:,0] + n_occ[:,1] + n_occ[:,2] + n_occ[:,3] + n_occ[:,6]
+    n_2ppi = n_occ[:,4] + n_occ[:,5]
+    n_2pz = n_occ[:,7]
+    n_4s = n_occ[:,8]
+    d = pd.DataFrame({'E':E,'Sz':Sz,'n_3d':n_3d,'n_2pz':n_2pz,'n_2ppi':n_2ppi,'n_4s':n_4s})
+    
+    E = res3[0]
+    n_occ = res3[2]+res3[3]
+    Sz = np.ones(len(E))*1.5
+    n_3d = n_occ[:,0] + n_occ[:,1] + n_occ[:,2] + n_occ[:,3] + n_occ[:,6]
+    n_2ppi = n_occ[:,4] + n_occ[:,5]
+    n_2pz = n_occ[:,7]
+    n_4s = n_occ[:,8]
+    d = pd.concat((d,pd.DataFrame({'E':E,'Sz':Sz,'n_3d':n_3d,'n_2pz':n_2pz,'n_2ppi':n_2ppi,'n_4s':n_4s})),axis=0)
+
+    d['E']-=min(d['E'])
+    d['eig']=np.arange(d.shape[0])
+    d['beta']=beta
+    if(full_df is None): full_df = d
+    else: full_df = pd.concat((full_df,d),axis=0)
+  sns.pairplot(full_df,vars=['E','n_2ppi','n_2pz','n_4s','n_3d'],hue='beta',markers=['o']+['.']*14)
+  plt.show() 
 
 if __name__=='__main__':
   df=collect_df()
   df=format_df(df)
-  '''
-  ind=np.argsort(df['energy'][df['basestate']==-1])
-  df=df[df['basestate']==-1].iloc[[0,1,2,3,4,10,11,12]].sort_values(by=['energy'])
-  print(df[['energy','mo_n_2pz','mo_n_2ppi','mo_n_3d','mo_n_4s','Sz','Jsd','Us']])
-  exit(0)
-  '''
   analyze(df,save=False)
--- qwalk/ub3lyp_s1_/ed.py
+++ qwalk/ub3lyp_s1_/ed.py
@@ -26,8 +26,8 @@ def h1_moToIAO(parms,printvals=False):
 
   #IAO ordering: ['del','del','yz','xz','x','y','z2','z','s'] 
   #MO ordering:  dxz, dyz, dz2, delta, delta, px, py, pz, 4s
-  ed,epi,epz,tpi,tdz,tsz,tds=parms
-  es=0
+  es,epi,epz,tpi,tdz,tsz,tds=parms
+  ed=0
 
   e=np.diag([ed,ed,ed,ed,ed,epi,epi,epz,es])
   e[[0,1,5,6],[5,6,0,1]]=tpi
@@ -52,29 +52,32 @@ def h1_moToIAO(parms,printvals=False):
 
   return e
 
-def h2_IAO(Jsd,Us):
+def h2_IAO(Jcu,Us):
   #Jsi = 0.25*(ns_u - ns_d)*(ni_u - ni_d) + 
   #0.5*(cs_u^+ cs_d ci_d^+ ci_u + cs_d^+ cs_u ci_u^+ ci_d)
-  
-  print(Jsd,Us)
 
   #PYSCF ordering: 2rdm[p,r,q,s]_x,x' = <cp_x^+ cq_x'^+ cs_x' cr_x>
   h2=np.zeros((3,9,9,9,9))
-  s=8 #index for 4s orbital
-  for i in [0,1,2,3,6]:
-    h2[0][s,s,i,i]=0.25
-    h2[2][s,s,i,i]=0.25
-    
-    h2[1][i,i,s,s]=-0.25
-    h2[1][s,s,i,i]=-0.25
-    h2[1][s,i,i,s]=-0.50
-    h2[1][i,s,s,i]=-0.50
-  h2*=Jsd
-  h2[1][s,s,s,s]=Us
+  index=[0,1,2,3,6,8]
+  #for p in range(len(index)):
+  #  for j in range(p+1,len(index)):
+  j=5
+  for p in range(len(index)-1):
+      s=index[p]
+      i=index[j]
+      h2[0][s,s,i,i]=0.25
+      h2[2][s,s,i,i]=0.25
+      
+      h2[1][i,i,s,s]=-0.25
+      h2[1][s,s,i,i]=-0.25
+      h2[1][s,i,i,s]=-0.50
+      h2[1][i,s,s,i]=-0.50
+  h2*=Jcu
+  h2[1][8,8,8,8]=Us
   return h2
 
 def ED(parms, nroots, norb, nelec):
-  h1=h1_moToIAO(parms[:-2],printvals=True)
+  h1=h1_moToIAO(parms[:-2])
   h1=np.array([h1,h1])  #SINGLE PARTICLE CHECK IS OK!
   h2=h2_IAO(parms[-2],parms[-1])
 
@@ -99,56 +102,53 @@ def ED(parms, nroots, norb, nelec):
   return e, ci, np.array(n_occ_u), np.array(n_occ_d)
 
 if __name__=='__main__':
-  nroots=20
+  '''
   norb=9
-  parms=(-3.3497,-0.2666,-1.8632,1.2426, 0.4035,0,-1.2825,-0.8105,3.6965)
-  #parms=(-3.0469,-0.8068,-2.0120,0.6164,0,0,0,-0.6505, 3.3954)
-  #parms=(-3.0036, -0.7844,-2.0565,0.6780,0,0,0, -0.6126, 3.424)
-  #parms=(-3.1228,-0.8323,-1.9754,0.5603,0,0,0,-0.7487,3.3377)
-  #parms=(-2.9821,-0.8129,-1.9137,0.5934,0,0,0,-0.5586,3.0822)
-  #parms=(-3.1875,-0.6328,-2.1335,1.1236,0,0.7267,-1.2768,-0.8416,3.2801)
-  #parms=(-2.9085,-0.6995,-2.1782,0.8505,0,0,0,-0.6070,3.5297)
-  #parms=(-3.0903,-0.7501,-1.6424,0.6930,0,0,0,-0.4296,0)
-  #parms=(-3.0314,-0.6640,-1.5162,0.6780,0,0,0,0,0)
 
-  nelec=(8,7)
-  res1=ED(parms,nroots,norb,nelec)
+  full_df=None
+  beta=0
+  for parms in [
+  (3.0019,2.2094,0.6435,0.8995,0,0,0,-0.5798,4.2049),
+  (3.0310,2.1832,0.7768,0.8089,0,0,0,-0.5531,4.0500),
+  (3.0589,2.1512,0.9370,0.6936,0,0,0,-0.5322,3.7743),
+  (3.0883,2.1305,1.0353,0.5985,0,0,0,-0.5459,3.6741),
+  (3.0982,2.1120,1.0622,0.5232,0,0,0,-0.5692,3.8343),
+  (3.0899,2.0986,1.0575,0.4635,0,0,0,-0.5529,4.0939),
+  (3.0686,2.0945,1.0467,0.4232,0,0,0,-0.4561,4.3103),
+  (3.0413,2.0918,1.0332,0.4002,0,0,0,-0.3107,4.4374)]:
 
-  nelec=(9,6)
-  res3=ED(parms,nroots,norb,nelec)
+    nelec=(8,7)
+    nroots=14
+    res1=ED(parms,nroots,norb,nelec)
 
-  E = res1[0]
-  n_occ_u = res1[2]
-  n_occ_d = res1[3]
-  Sz = np.ones(len(E))*0.5
-  n_3d_u = n_occ_u[:,0] + n_occ_u[:,1] + n_occ_u[:,2] + n_occ_u[:,3] + n_occ_u[:,6]
-  n_2ppi_u = n_occ_u[:,4] + n_occ_u[:,5]
-  n_2pz_u = n_occ_u[:,7]
-  n_4s_u = n_occ_u[:,8]
-  n_3d_d = n_occ_d[:,0] + n_occ_d[:,1] + n_occ_d[:,2] + n_occ_d[:,3] + n_occ_d[:,6]
-  n_2ppi_d = n_occ_d[:,4] + n_occ_d[:,5]
-  n_2pz_d = n_occ_d[:,7]
-  n_4s_d = n_occ_d[:,8]
-  df = pd.DataFrame({'E':E,'Sz':Sz,'n_3d_u':n_3d_u,'n_2pz_u':n_2pz_u,'n_2ppi_u':n_2ppi_u,'n_4s_u':n_4s_u,
-  'n_3d_d':n_3d_d,'n_2pz_d':n_2pz_d,'n_2ppi_d':n_2ppi_d,'n_4s_d':n_4s_d})
-  
-  E = res3[0]
-  n_occ_u = res3[2]
-  n_occ_d = res3[3]
-  Sz = np.ones(len(E))*1.5
-  n_3d_u = n_occ_u[:,0] + n_occ_u[:,1] + n_occ_u[:,2] + n_occ_u[:,3] + n_occ_u[:,6]
-  n_2ppi_u = n_occ_u[:,4] + n_occ_u[:,5]
-  n_2pz_u = n_occ_u[:,7]
-  n_4s_u = n_occ_u[:,8]
-  n_3d_d = n_occ_d[:,0] + n_occ_d[:,1] + n_occ_d[:,2] + n_occ_d[:,3] + n_occ_d[:,6]
-  n_2ppi_d = n_occ_d[:,4] + n_occ_d[:,5]
-  n_2pz_d = n_occ_d[:,7]
-  n_4s_d = n_occ_d[:,8]
-  df = pd.concat((df,pd.DataFrame({'E':E,'Sz':Sz,'n_3d_u':n_3d_u,'n_2pz_u':n_2pz_u,'n_2ppi_u':n_2ppi_u,'n_4s_u':n_4s_u,
-  'n_3d_d':n_3d_d,'n_2pz_d':n_2pz_d,'n_2ppi_d':n_2ppi_d,'n_4s_d':n_4s_d})),axis=0)
+    nelec=(9,6)
+    nroots=6
+    res3=ED(parms,nroots,norb,nelec)
 
-  df['E']-=min(df['E'])
-  print(df.sort_values(by=['E']))
-  plt.plot(df['E'][df['Sz']==0.5],'go')
-  plt.plot(df['E'][df['Sz']==1.5],'ro')
+    E = res1[0]
+    n_occ = res1[2]+res1[3]
+    Sz = np.ones(len(E))*0.5
+    n_3d = n_occ[:,0] + n_occ[:,1] + n_occ[:,2] + n_occ[:,3] + n_occ[:,6]
+    n_2ppi = n_occ[:,4] + n_occ[:,5]
+    n_2pz = n_occ[:,7]
+    n_4s = n_occ[:,8]
+    df = pd.DataFrame({'E':E,'Sz':Sz,'n_3d':n_3d,'n_2pz':n_2pz,'n_2ppi':n_2ppi,'n_4s':n_4s})
+    
+    E = res3[0]
+    n_occ = res3[2]+res3[3]
+    Sz = np.ones(len(E))*1.5
+    n_3d = n_occ[:,0] + n_occ[:,1] + n_occ[:,2] + n_occ[:,3] + n_occ[:,6]
+    n_2ppi = n_occ[:,4] + n_occ[:,5]
+    n_2pz = n_occ[:,7]
+    n_4s = n_occ[:,8]
+    df = pd.concat((df,pd.DataFrame({'E':E,'Sz':Sz,'n_3d':n_3d,'n_2pz':n_2pz,'n_2ppi':n_2ppi,'n_4s':n_4s})),axis=0)
+
+    df['E']-=min(df['E'])
+    df['eig']=np.arange(df.shape[0])
+    df['beta']=beta
+    if(full_df is None): full_df = df
+    else: full_df = pd.concat((full_df,df),axis=0)
+    beta+=0.5
+  sns.pairplot(full_df,vars=['E','n_3d','n_2pz','n_2ppi','n_4s'],hue='beta',markers=['o']+['.']*7)
   plt.show()
+  '''
