7056b1496ed72f81d4ae106e6d4e55d4d29c16fa
Shivesh Ananda Pathak <sapatha2@h2ologin1.ncsa.illinois.edu>
2019-05-11 21:22:23 -0500

    finished extra runs, maybe these can help with our models!

---
+++ qwalk/ub3lyp_extra_extra/copy_files.py
@@ -0,0 +1,10 @@
+import os 
+import numpy as np 
+
+for basestate in np.arange(4):
+  for gsw in np.arange(0.2,1.2,0.2):
+    src_dir = '~/scratch/cuo/qwalk/ub3lyp_extra_extra/gsw'+str(np.around(gsw,2))+'b'+str(basestate)
+    dest_dir = 'gsw'+str(np.around(gsw,2))+'b'+str(basestate)
+    print(src_dir, dest_dir)
+    os.system('cp '+src_dir+'/*.o ./'+(dest_dir))
+    os.system('cp '+src_dir+'/*.log ./'+str(dest_dir))
